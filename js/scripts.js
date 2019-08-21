f909a5d9a121d91879f517104dd1853a04456021
Andrew Peterson and Molly Stambaugh <molly@>
2019-08-21 14:55:27 -0700

    add a bunch of stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,45 @@
+//BUSINESS LOGIC
 
 var vowel = ["a", "e", "i", "o", "u"]
+
+function masterpig (phrase) {
+  lowercasephrase = phrase.toLowerCase();
+  var words = lowercasephrase.split(" ");
+  var pigLatin = " ";
+  words.forEach (function (sentence) {
+    pigLatin+= pigLatinTranslator (sentence);
+    pigLatin+= " ";
+  });
+  return pigLatin[0].toUpperCase() + pigLatin.slice(1)
+}
+
+
+function pigLatinTranslator(sentence) {
+  if (vowelFinder(sentence[index]) === true) {
+    for (var index = 0; index < word.length; index += 1) {
+      if (index > 0 && word[index] === "u" && word[index - 1] === "q") {
+        
+      }
+    }
+  }
+}
+
+function vowelFinder(letter) {
+  for (var index = 0; index <= vowel.length; index += 1) {
+    if (letter === vowel[index]) {
+      return true
+    } else {
+      return false;
+    }
+  }
+};
+
+//FRONTEND LOGIC
 
 $(document).ready(function(){
   $("form#latinForm").submit(function() {
     event.preventDefault();
     var userInput = $("input#sentence").val();
-    // console.log(userInput);
-    var sentenceArray = userInput.split("");
-    // console.log(sentenceArray);
-
-    for (var index = 0; index <= sentenceArray; index += 1) {
-      if (newArray[index])
-    }
+    // var sentenceArray = userInput.split("");
   });
 });
