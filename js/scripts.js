f0817ffc7d786986c5c6f8f8b2b42029146a57b2
Andrew Peterson and Molly Stambaugh <andrew@>
2019-08-21 16:45:35 -0700

    add to css and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //BUSINESS LOGIC
 
-var vowel = ["a", "e", "i", "o", "u"]
+var vowel = ['a', 'e', 'i', 'o', 'u']
 
 function masterpig (phrase) {
-  lowercasephrase = phrase.toLowerCase();
+  var lowercasephrase = phrase.toLowerCase();
   var words = lowercasephrase.split(" ");
-  var pigLatin = " ";
-  words.forEach (function (sentence) {
-    pigLatin+= pigLatinTranslator (sentence);
+  var pigLatin = "";
+  words.forEach (function(sentence) {
+    pigLatin+= pigLatinTranslator(sentence);
     pigLatin+= " ";
   });
   return pigLatin[0].toUpperCase() + pigLatin.slice(1)
@@ -15,31 +15,35 @@ function masterpig (phrase) {
 
 
 function pigLatinTranslator(sentence) {
-  if (vowelFinder(sentence[index]) === true) {
-    for (var index = 0; index < word.length; index += 1) {
-      if (index > 0 && word[index] === "u" && word[index - 1] === "q") {
-        
+  if (vowelFinder(sentence[0]) === true)return sentence + "way"
+  for (var index = 0; index < sentence.length; index = index += 1) {
+    if (vowelFinder(sentence[index]) === true) {
+      if (index > 0 && sentence[index] === "u" && sentence[index - 1] === "q") {
+        //                      "ite"           +    "qu"               + "ay"
+        return sentence.slice(index + 1) + sentence.slice(0, index + 1) + "ay"
+      } else {
+        return sentence.slice(index) + sentence.slice(0, index) + "ay"
       }
     }
   }
+  return sentence;
 }
 
 function vowelFinder(letter) {
-  for (var index = 0; index <= vowel.length; index += 1) {
-    if (letter === vowel[index]) {
-      return true
-    } else {
-      return false;
-    }
+  for (var index = 0; index < vowel.length; index += 1) {
+    if (letter === vowel[index])return true;
   }
-};
+  return false;
+}
 
 //FRONTEND LOGIC
 
 $(document).ready(function(){
   $("form#latinForm").submit(function() {
     event.preventDefault();
-    var userInput = $("input#sentence").val();
-    // var sentenceArray = userInput.split("");
+    // var userInput = $("input#sentence").val();
+
+    $("p#results").text(masterpig($("input#sentence").val()));
+
   });
 });
